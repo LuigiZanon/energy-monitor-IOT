be8ef3a8e8aec3d180da525cc651eba05c2717a6
Guilherme Andrade <guiandradeemail@gmail.com>
2026-03-19 21:17:55 -0300

    feat: using wifi manager lib

--- test_wifi/main.ino
+++ test_wifi/main.ino
@@ -1,258 +1,232 @@
 #include <WiFi.h>
-#include <WebServer.h>
+#include <WiFiManager.h>         
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <PubSubClient.h>
+#include <Adafruit_ADS1X15.h>
 
 #define LARGURA_TELA 128
-#define ALTURA_TELA 64
+#define ALTURA_TELA 32
 #define PINO_RESET -1
+#define MAX_BATCH 10
+#define BTN_RESET 5 // Valor aleatório enquanto nao coloca
 
-WebServer server(80);
+TwoWire I2C_ADS = TwoWire(1);
 Adafruit_SSD1306 display(LARGURA_TELA, ALTURA_TELA, &Wire, PINO_RESET);
+WiFiManager wm;
+WiFiClient espClient;
+PubSubClient client(espClient);
 
-void setup_esp();
-void connectWifi();
-void run();
-void verifyDevices();
-void updateDisplayConfig();
-void resetConfigMode();
+const char* SSID = "EnergyMonitor"; 
+const char* password = "";
+const char* mqtt_server = "192.168.68.115";
+const char* mqtt_user = "";
+const char* mqtt_password = "";
+const char* mqtt_topic = "teste/esp";
+const int mqtt_port = 1883;
 
-enum State {
-  CONFIG,       // abre Wi-Fi + servidor
-  CONNECTING_WIFI,   // tenta conectar
-  RUNNING        // roda aplicação
+Adafruit_ADS1115 ads;
+const float RESISTOR_BURDEN = 22.0; 
+const float RELACAO_SENSOR = 2000.0; 
+const float FATOR_TENSAO_ADS = 0.125; 
+
+struct __attribute__((packed)) Sample {
+  float current;
 };
 
-State state = CONFIG;
+Sample buffer[MAX_BATCH];
+int count = 0;
+void addData(float current) {
+    buffer[count].current = current;
+    count++;
 
-const char* espSSID = "ESP_Config";
-const char* espPassword = "";
+    if (count == MAX_BATCH) {
+        // client.publish(mqtt_topic, (uint8_t*)buffer, sizeof(buffer));
+        client.publish(mqtt_topic, (uint8_t*)buffer, count * sizeof(Sample));
+        count = 0;
+    }
+}
 
-// ===== CONFIG DO ACCESS POINT =====
-String homeSSID = "";
-String homePassword = "";
+// ===== CALLBACK MQTT =====
+void callback(char* topic, byte* payload, unsigned int length) {
+  // Serial.print("Mensagem recebida [");
+  // Serial.print(topic);
+  // Serial.print("]: ");
 
-int connectedDevices = -1;
+  // for (int i = 0; i < length; i++) {
+  //   Serial.print((char)payload[i]);
+  // }
+  // Serial.println();
+}
 
-// ===== SETUP =====
+// ===== RECONEXÃO MQTT =====
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Conectando ao MQTT...");
+
+    String clientId = "ESPClient-";
+    clientId += String(random(0xffff), HEX);
+
+    if (client.connect(clientId.c_str())) {
+      Serial.println("conectado!");
+
+      // Subscribe em um tópico
+      client.subscribe(mqtt_topic);
+
+    } else {
+      Serial.print("falhou, rc=");
+      Serial.print(client.state());
+      Serial.println(" tentando novamente em 5 segundos");
+      delay(5000);
+    }
+  }
+}
+
+
 void setup() {
   Serial.begin(115200);
 
-  Wire.begin(26, 25);    // I2C0: SDA = 25, SCL = 26 (Para o Display)
+  // pinMode(BTN_RESET, INPUT);
+
+  // Configuração dos I2Cs
+  I2C_ADS.begin(33, 32); // I2C1: SDA = 32, SCL = 33 (Para o ADS1115)
+  Wire.begin(26, 25);
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println("Falha ao inicializar o SSD1306!");
-    while (1);
+    while(1);
   }
+
+  if (!ads.begin(0x48, &I2C_ADS)) {
+    Serial.println("Falha ao inicializar o ADS1115!");
+    display.clearDisplay();
+    display.setCursor(10, 20);
+    display.println("Erro no ADS1115!");
+    display.display();
+    while (1); 
+  }
+
+  ads.setGain(GAIN_ONE);
+  ads.setDataRate(RATE_ADS1115_860SPS); 
+
+  // Configuração do Wifi
+
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
-  display.setCursor(10, 20);
+  display.setCursor(0, 0);
   display.println("Iniciando...");
   display.display();
   delay(1000);
 
-  setup_esp();
+  // wm.resetSettings();
+
+  wm.setAPCallback([](WiFiManager* wm){
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setCursor(0,0);
+    display.println("Modo Configuracao");
+    display.print("WiFi: ");
+    display.println(SSID);
+    display.print("IP: ");
+    display.println(WiFi.softAPIP()); // agora o IP estará correto
+    display.display();
+  });
+
+  // Tenta conectar ao Wi-Fi salvo ou abre portal cativo
+  if(!wm.autoConnect(SSID, password)) {
+    Serial.println("Falhou conectar e timeout do portal cativo");
+    display.clearDisplay();
+    display.setCursor(0, 10);
+    display.println("Falha WiFi");
+    display.display();
+    delay(3000);
+    ESP.restart();
+  }
+
+  Serial.println("Wi-Fi conectado, iniciando MQTT...");
+  client.setServer(mqtt_server, mqtt_port);
+
+  client.setCallback(callback);
+
+  Serial.println("Conectado ao WiFi!");
 }
 
-// ===== LOOP =====
+// --- FUNÇÃO PARA CALCULAR A CORRENTE RMS ---
+float calcularCorrenteRMS(uint32_t tempoAmostragem) {
+  uint32_t tempoInicio = millis();
+  int32_t valorMaximo = 0;
+  int32_t valorMinimo = 65535; 
+  int16_t leituraADC = 0;
+
+  while ((millis() - tempoInicio) < tempoAmostragem) {
+    leituraADC = ads.readADC_SingleEnded(0); 
+    if (leituraADC > valorMaximo) { valorMaximo = leituraADC; }
+    if (leituraADC < valorMinimo) { valorMinimo = leituraADC; }
+  }
+
+  float tensaoPicoPico = ((valorMaximo - valorMinimo) * FATOR_TENSAO_ADS) / 1000.0;
+  float tensaoRMS = (tensaoPicoPico / 2.0) * 0.707;
+  float correnteSecundaria = tensaoRMS / RESISTOR_BURDEN;
+  float correnteReal = correnteSecundaria * RELACAO_SENSOR;
+  
+  if (correnteReal < 0.15) { correnteReal = 0.0; }
+
+  return correnteReal;
+}
+
 void loop() {
-  switch(state) {
-    case CONFIG:
-      server.handleClient();
-      verifyDevices();
-      break;
-
-    case CONNECTING_WIFI:
-      connectWifi();
-      break;
-
-    case RUNNING:
-      run();
-      break;
+  // Código da sua aplicação
+  if (!client.connected()) {
+    reconnect();
   }
-  
-}
 
-// ========= MODO DE CONFIGURAÇÃO
-String htmlPage = R"rawliteral(
-<!DOCTYPE html>
-<html>
-<head>
-  <title>ESP Setup</title>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <style>
-    body { font-family: Arial; text-align: center; }
-    input { padding: 10px; margin: 5px; width: 80%; }
-    button { padding: 10px 20px; }
-  </style>
-</head>
-<body>
-  <h2>Configurar WiFi</h2>
-  <form action="/save">
-    <input type="text" name="ssid" placeholder="SSID"><br>
-    <input type="password" name="pass" placeholder="Senha"><br>
-    <button type="submit">Salvar</button>
-  </form>
-</body>
-</html>
-)rawliteral";
+  client.loop();
+  unsigned long now = millis();
 
-void handleRoot() {
-  server.send(200, "text/html", htmlPage);
-}
+  static unsigned long lastDisplay = 0;
 
-void handleSave() {
-  homeSSID = server.arg("ssid");
-  homePassword = server.arg("pass");
+  // Faz a leitura da corrente
+  float corrente = calcularCorrenteRMS(200); 
 
-  Serial.println("Novo SSID: " + homeSSID);
-  Serial.println("Nova Senha: " + homePassword);
-
-  state = CONNECTING_WIFI;
-
-  server.send(200, "text/html", "<h1>Salvo! Reinicie o dispositivo.</h1>");
-}
-
-void setup_esp() {
-  // Verificar se existe salvo no SD
-
-  // Cria o Access Point
-  WiFi.softAP(espSSID, espPassword);
-
-  Serial.println("AP criado!");
-  Serial.print("IP: ");
-  Serial.println(WiFi.softAPIP());
-
-  // Rotas
-  server.on("/", handleRoot);
-  server.on("/save", handleSave);
-
-  server.begin();
-  Serial.println("Servidor HTTP iniciado");
-  
-  connectedDevices = WiFi.softAPgetStationNum();
-  updateDisplayConfig();
-
-}
-
-void updateDisplayConfig() {
-  display.clearDisplay();
-
-  if (connectedDevices == 0) {
-
+  if (now - lastDisplay > 500) {
+    display.clearDisplay(); 
+    
     display.setTextSize(1);
     display.setCursor(0, 0);
-    display.print("Conecte-se ao wifi");
-
+    display.print("Corrente (SCT-013):");
+    
+    display.setTextSize(3); 
+    display.setCursor(0, 10);
+    display.print(corrente, 2); 
+    
     display.setTextSize(2);
-    display.setCursor(0, 25);
-    display.print(espSSID);
+    display.print(" A");
+    
+    display.display(); 
+    
+    Serial.print("Corrente: ");
+    Serial.print(corrente, 2);
+    Serial.println(" A");
 
-  } else {
-
-    display.setTextSize(1);
-    display.setCursor(0, 0);
-    display.print("Acesse o IP:");
-
-    display.setTextSize(2);
-    display.setCursor(0, 25);
-    display.print(WiFi.softAPIP());
-
+    addData(corrente);
   }
 
-  display.display();
-}
+  // if(digitalRead(BTN_RESET) == LOW){
+  //     WiFiManager wm;
+  //     wm.resetSettings();
+  //     ESP.restart();
+  // }
+  
+  // // Publica mensagem a cada 5s
+  // static unsigned long lastMsg = 0;
 
-void verifyDevices() {
-  int count = WiFi.softAPgetStationNum();
+  // if (now - lastMsg > 5000) {
+  //   lastMsg = now;
 
-  if (count != connectedDevices) {
-    connectedDevices = count;
-    updateDisplayConfig();
-  }
-}
+  //   String msg = "Olá do ESP!";
+  //   Serial.print("Publicando: ");
+  //   Serial.println(msg);
 
-unsigned long lastAttempt = 0;
-int retries = 0;
-
-void resetConfigMode() {
-  Serial.println("Voltando para modo CONFIG");
-
-  // Para Wi-Fi atual (station)
-  WiFi.disconnect(true);
-
-  // Desliga AP antigo (se existir)
-  WiFi.softAPdisconnect(true);
-
-  delay(500);
-
-  // Reseta variáveis
-  connectedDevices = -1;
-
-  // Sobe AP novamente
-  WiFi.softAP(espSSID, espPassword);
-
-  Serial.println("AP recriado!");
-  Serial.print("IP: ");
-  Serial.println(WiFi.softAPIP());
-
-  // Atualiza estado
-  state = CONFIG;
-
-  // Atualiza display
-  updateDisplayConfig();
-}
-
-void connectWifi() {
-  if (retries == 0) {
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(homeSSID.c_str(), homePassword.c_str());
-
-    display.clearDisplay();
-    display.setTextSize(2);
-    display.setCursor(0, 25);
-    display.print("Conectando...");
-    display.display();
-  }
-
-  if (millis() - lastAttempt > 500) {
-    lastAttempt = millis();
-    retries++;
-    Serial.print(".");
-  }
-
-  if (WiFi.status() == WL_CONNECTED) {
-    Serial.println("Conectado!");
-    state = RUNNING;
-    retries = 0;
-  }
-
-  if (retries >= 20) {
-    Serial.println("Falhou!");
-
-    display.clearDisplay();
-    display.setCursor(0, 0);
-    display.print("Rede incorreta");
-
-    display.setCursor(0, 20);
-    display.print("Tente novamente");
-
-    display.display();
-
-    delay(1500);
-
-    retries = 0;
-    resetConfigMode();
-  }
-}
-
-// == MODO DE EXECUÇÃO
-void run(){
-  display.clearDisplay();
-
-  display.setCursor(0, 20);
-  display.print("Executando");
-
-  display.display();
+  //   client.publish(mqtt_topic, msg.c_str());
+  // }
 }
