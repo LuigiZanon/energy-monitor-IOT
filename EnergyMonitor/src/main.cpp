33be5370956a23498a158894201c033687305728
PG2zinho <pedro-gasparotto@outlook.com>
2026-03-29 18:09:23 -0300

    Inicial test to measure AC tension

--- EnergyMonitor/src/main.cpp
+++ EnergyMonitor/src/main.cpp
@@ -1,9 +1,83 @@
+/*#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_ADS1X15.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+TwoWire I2C_ads = TwoWire(1);
+
+Adafruit_ADS1115 ads;
+
+void setup() {
+  Serial.begin(115200);
+  
+  delay(1000); // Aguarda o monitor serial iniciar
+  I2C_ads.begin(32, 33); // sda, scl
+
+  if (!ads.begin(0x48, &I2C_ads)) {
+    Serial.println("Failed to initialize ADS1115!");
+    Serial.println("Please check your wiring and I2C address.");
+    while (1);
+  }
+
+  // CONFIGURAÇÃO DE GANHO:
+  // GAIN_ONE: +/- 4.096V (Ideal para leitura de 3.3V)
+  // 1 bit = 0.125mV
+  ads.setGain(GAIN_ONE);
+
+  Serial.println("--- Iniciando Calibracao de Offset ---");
+  Serial.println("Certifique-se de que NAO ha tensao AC no sensor.");
+  delay(2000);
+}
+
+void loop() {
+  
+  long somaRaw = 0;
+  float somaVolts = 0;
+  const int numeroAmostras = 10000;
+
+  for (int i = 0; i < numeroAmostras; i++) {
+    int16_t adc0 = ads.readADC_SingleEnded(1); // Lendo pino A1
+    somaRaw += adc0;
+    somaVolts += ads.computeVolts(adc0);
+    delay(1); // Pequeno delay para estabilidade
+    Serial.println(adc0);
+  }
+
+  float offsetMedioRaw = (float)somaRaw / numeroAmostras;
+  float offsetMedioVolts = somaVolts / numeroAmostras;
+
+  Serial.print("Offset Medio (Raw): ");
+  Serial.println(offsetMedioRaw);
+  Serial.print("Offset Medio (Volts): ");
+  Serial.print(offsetMedioVolts, 4); 
+  Serial.println("Use este valor de 'Volts' no seu codigo de RMS.");
+  
+  delay(10000); // Repete a cada 5 segundos
+}*/
+
+//float CalculoTesao(){
+/*TODO
+  Descobrir como a leitura do ZMPT101B funciona
+  Fazer ele se conectar com o ADS
+  Executar o calculo da tensao 
+  REZAR!
+  //offsetRaw:12681.42
+  //Meu offset: 1.6100 V
+*/
+
+
+//}
+
+
+
 #include <Arduino.h>
 #include <Wire.h>
 #include <Adafruit_ADS1X15.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include "sensor.h"
+#include "display.h"
 
 TwoWire I2C_ads = TwoWire(1);
 
@@ -38,6 +112,6 @@ void setup() {
 }
 
 void loop() {
-  Serial.println("Estou vivo!");
-  delay(1000);
+  
+  vTaskDelete(NULL);
 }
--- EnergyMonitor/src/sensor.cpp
+++ EnergyMonitor/src/sensor.cpp
@@ -5,6 +5,7 @@
 
 // Se o sensor for o SCT-013 100A/1V, a relação é 100 Amperes por 1 Volt
 #define FATOR_CONVERSAO_SCT 100.0 
+#define FATOR_CONVERSAO_ZMPT 610.0
 
 #define FATOR_CALIBRACAO_FINA 1.00 
 
@@ -12,24 +13,33 @@ short ciclos = 2;
 
 const uint32_t TEMPO_AMOSTRAGEM_US = (1000000 / 60) * ciclos;
 
+
 void TaskCurrentSensor(void *pvParameters)
 {
-    static double offsetDC = 13200.0;
+    static double offsetDC_SCT = 13200.0;
+    static double offset_ZMPT = 12681.42;
 
     for (;;)
     {
         uint32_t tempoInicio = micros();
-        double somaQuadrados = 0;
+        double somaQuadrados_SCT = 0;
         uint32_t totalAmostras = 0;
+        double somaQuadrados_ZMPT = 0;
 
         while ((micros() - tempoInicio) < TEMPO_AMOSTRAGEM_US)
         {
-            int16_t sample = ads.readADC_SingleEnded(0);
+            int16_t sample_SCT = ads.readADC_SingleEnded(0);
+            int16_t sample_ZMPT = ads.readADC_SingleEnded(1);
 
-            offsetDC = offsetDC + ((sample - offsetDC) / 1024.0);
-            double sinalFiltrado = sample - offsetDC;
+            offsetDC_SCT = offsetDC_SCT + ((sample_SCT - offsetDC_SCT) / 1024.0);
+            offset_ZMPT = offset_ZMPT + ((sample_ZMPT - offset_ZMPT) / 1024.0);
 
-            somaQuadrados += (sinalFiltrado * sinalFiltrado);
+
+            double sinalFiltrado_SCT = sample_SCT - offsetDC_SCT;
+            double sinalFiltrado_ZMPT = sample_ZMPT - offset_ZMPT;
+
+            somaQuadrados_SCT += (sinalFiltrado_SCT * sinalFiltrado_SCT);
+            somaQuadrados_ZMPT += (sinalFiltrado_ZMPT * sinalFiltrado_ZMPT);
             totalAmostras++;
 
             taskYIELD(); 
@@ -37,19 +47,23 @@ void TaskCurrentSensor(void *pvParameters)
 
         if (totalAmostras > 0)
         {
-            double rmsADC = sqrt(somaQuadrados / totalAmostras);
+            double rmsADC = sqrt(somaQuadrados_SCT / totalAmostras);
             float correnteRMS = (rmsADC * FATOR_TENSAO_ADS) / 1000.0;
-
             float correnteTeorica = correnteRMS * FATOR_CONVERSAO_SCT;
-            
             float correnteReal = correnteTeorica * FATOR_CALIBRACAO_FINA;
+
+            double rmsADC_ZMPT = sqrt(somaQuadrados_ZMPT / totalAmostras);
+            float vRMS_ZMPT_Baixa = (rmsADC_ZMPT * FATOR_TENSAO_ADS) / 1000.0;
+            float tensaoReal = vRMS_ZMPT_Baixa * FATOR_CONVERSAO_ZMPT;
 
             if (correnteReal < 0.05) {
                 correnteReal = 0.00;
             }
 
-            Serial.print(">Corrente:");
-            Serial.println(correnteReal);
+            //Serial.print(">Corrente:");
+            //Serial.println(correnteReal);
+            Serial.print(">Volts");
+            Serial.println(tensaoReal);
         }
 
         vTaskDelay(pdMS_TO_TICKS(5));
