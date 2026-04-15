7f0556c7ba31a7c0e849e6c92c2b9d6bf278b532
Isabela Crestana <isabela.everton@onerf.com.br>
2026-04-15 12:11:23 -0300

    Change in zmp offset

--- EnergyMonitor/include/sensor.h
+++ EnergyMonitor/include/sensor.h
@@ -6,7 +6,7 @@
 #include <Adafruit_ADS1X15.h>
 
 #define OFFSET_SCT 13200.0
-#define OFFSET_ZMPT 13045.0f
+#define OFFSET_ZMPT 13029.46f
 
 //  Declaracao do ads, extern pois e definido em main.cpp
 extern Adafruit_ADS1115 ads;
